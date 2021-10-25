e6c236a642dc691892fdbdb7e79aff32934bdeaa
Ashur Cabrera <hello@ashur.cab>
2021-10-24 20:47:47 -0600

    Add initial Install/Step implementation

---
+++ src/_data/components/Install/Step1.js
@@ -0,0 +1,22 @@
+module.exports = {
+	id: "Phone-Install-Step-1",
+	aspectRatio: 0.875,
+	caption: "Library search screen in Libby",
+
+	autoplay: false,
+	fade: true,
+	hideCaption: true,
+	pointer: false,
+	printedEffect: true,
+
+	frames: [
+		{
+			screens: {
+				bottom: {
+					background: "install/step-01-01",
+					opacity: "1"
+				},
+			},
+		},
+	],
+}
---
+++ src/_data/components/Install/Step2.js
@@ -0,0 +1,64 @@
+const FrameWrangler = require( "../../../utils/frame-wrangler" );
+
+let frameWrangler = new FrameWrangler({
+	screens: {
+		top: {
+			background: "install/step-02-02",
+			opacity: 0
+		},
+		bottom: {
+			background: "install/step-02-01",
+			opacity: 1
+		},
+
+		pointer: {
+			x: 0.3,
+			y: 0.6,
+		}
+	},
+
+	duration: 1000,
+});
+
+frameWrangler.tap();
+frameWrangler.showTopScreen();
+
+frameWrangler.movePointer( 0.4, 0.0875, 1250 );
+frameWrangler.pause( 250 );
+frameWrangler.setBottomScreenBackground( "install/step-02-03" );
+frameWrangler.tap();
+frameWrangler.hideTopScreen();
+
+frameWrangler.pause( 250 );
+frameWrangler.setTopScreenBackground( "install/step-02-04" );
+frameWrangler.tap();
+frameWrangler.showTopScreen();
+
+frameWrangler.movePointer( 0.3, 0.175, 1000 );
+frameWrangler.pause( 250 );
+frameWrangler.tap();
+frameWrangler.hideTopScreen();
+
+frameWrangler.movePointer( 0.72, 0.09, 1000 );
+frameWrangler.pause( 125 );
+frameWrangler.setTopScreenBackground( "install/step-02-01" );
+frameWrangler.tap();
+frameWrangler.showTopScreen();
+
+frameWrangler.setBottomScreenBackground( "install/step-02-01" );
+frameWrangler.hideTopScreen();
+
+module.exports = {
+	id: "Phone-Install-Step-2",
+	aspectRatio: 1.125,
+	caption: "Library search screen in Libby",
+
+	autoplay: true,
+	fade: true,
+	hideCaption: true,
+	loop: false,
+	pointer: true,
+	printedEffect: true,
+
+	frames: frameWrangler.frames,
+}
