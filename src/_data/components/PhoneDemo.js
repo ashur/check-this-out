d6e2f02e85dac6e62a0063a78ce45a19da45d6ba
Ashur Cabrera <hello@ashur.cab>
2021-10-27 19:35:23 -0600

    Update PhoneDemo timing

--- src/_data/components/PhoneDemo.js
+++ src/_data/components/PhoneDemo.js
@@ -20,27 +20,27 @@ let frameWrangler = new FrameWrangler({
 
 frameWrangler.pause( 1000 );
 frameWrangler.tapDown( 750 );
-frameWrangler.movePointer( 0.4, null, 1000 );
+frameWrangler.movePointer( 0.4, null, 900 );
 frameWrangler.showTopScreen();
 
 frameWrangler.setBottomScreenBackground( "screen-gideon-3" );
-frameWrangler.tapUp( 250 );
+frameWrangler.tapUp( 150 );
 frameWrangler.hideTopScreen();
 
 frameWrangler.setTopScreenBackground( "screen-gideon-4" );
 frameWrangler.movePointer( null, 0.33, 1000 );
-frameWrangler.tap( 250 );
+frameWrangler.tap( 150 );
 frameWrangler.showTopScreen();
 
 frameWrangler.setBottomScreenBackground( "screen-gideon-5" );
 frameWrangler.pause( 500 );
 frameWrangler.movePointer( 0.13, 0.735, 1500 );
-frameWrangler.tap( 250 );
+frameWrangler.tap( 150 );
 frameWrangler.hideTopScreen();
 
 frameWrangler.movePointer( 0.425, 0.582, 1500 );
 frameWrangler.setTopScreenBackground( "screen-gideon-6" );
-frameWrangler.tap( 250 );
+frameWrangler.tap( 150 );
 frameWrangler.showTopScreen();
 frameWrangler.setBottomScreenBackground( "screen-gideon-1" );
 frameWrangler.pause( 1000 );
