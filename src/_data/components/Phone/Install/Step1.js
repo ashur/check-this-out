343326b88705db49df3840f5fe2627826534dc10
Ashur Cabrera <hello@ashur.cab>
2021-11-01 10:09:49 -0600

    Update Install/Step1 script

--- src/_data/components/Phone/Install/Step1.js
+++ src/_data/components/Phone/Install/Step1.js
@@ -1,22 +1,53 @@
+const FrameWrangler = require( "../../../../utils/frame-wrangler" );
+
+let frameWrangler = new FrameWrangler({
+	screens: {
+		top: {
+			background: "install/safari-tap-hold",
+			opacity: 0
+		},
+		bottom: {
+			background: "install/safari-install-step1",
+			opacity: "1"
+		},
+
+		pointer: {
+			x: 0.7125,
+			y: 0.825,
+		},
+	},
+});
+
+frameWrangler.movePointer( 0.42, 0.28, 1500 );
+
+frameWrangler.tapDown( 500 );
+frameWrangler.showTopScreen();
+frameWrangler.tapUp( 500 );
+
+frameWrangler.setBottomScreenBackground( "install/safari-tabs" );
+frameWrangler.movePointer( 0.2, 0.78, 1500 );
+frameWrangler.tap( 500 );
+
+frameWrangler.hideTopScreen();
+frameWrangler.pause( 500 );
+frameWrangler.setTopScreenBackground( "install/safari-libby-search" );
+frameWrangler.pause( 300 );
+frameWrangler.showTopScreen();
+
+frameWrangler.pause( 300 );
+frameWrangler.setBottomScreenBackground( "install/safari-install-step1" );
+frameWrangler.pause( 1000 );
+
 module.exports = {
 	id: "Phone-Install-Step-1",
-	aspectRatio: 0.95,
+	aspectRatio: 1.85,
 	caption: "Library search screen in Libby",
 
 	autoplay: false,
 	fade: true,
 	hideCaption: true,
-	pointer: false,
+	pointer: true,
 	printedEffect: true,
 
-	frames: [
-		{
-			screens: {
-				bottom: {
-					background: "install/step-01-01",
-					opacity: "1"
-				},
-			},
-		},
-	],
+	frames: frameWrangler.frames,
 }
