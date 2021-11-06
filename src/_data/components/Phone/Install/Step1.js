bca1e2e3d89bc2babe496103d8878ab2b58e999b
Ashur Cabrera <hello@ashur.cab>
2021-11-06 15:35:32 -0600

    Adjust Step 1 metrics

--- src/_data/components/Phone/Install/Step1.js
+++ src/_data/components/Phone/Install/Step1.js
@@ -19,29 +19,27 @@ let frameWrangler = new FrameWrangler({
 });
 
 frameWrangler.pause( 1000 );
-frameWrangler.movePointer( 0.37, 0.8175, 1000 );
+frameWrangler.movePointer( 0.37, 0.8475, 1000 );
 frameWrangler.tap();
 
 // Safari share sheet
 frameWrangler.showTopScreen();
-frameWrangler.movePointer( 0.125, 0.63125, 1000 );
+frameWrangler.movePointer( 0.125, 0.655, 1000 );
 frameWrangler.setBottomScreenBackground( "install/step-01-03" );
 frameWrangler.tap();
 
 // Add Bookmark
 frameWrangler.hideTopScreen();
 frameWrangler.setTopScreenBackground( "install/step-01-01" );
-frameWrangler.movePointer( 0.7375, 0.05125, 1175 );
+frameWrangler.movePointer( 0.7375, 0.055, 1175 );
 frameWrangler.tap();
 frameWrangler.showTopScreen();
 frameWrangler.setBottomScreenBackground( "install/step-01-01" );
 frameWrangler.hideTopScreen();
 
-frameWrangler.pause( 500 );
-
 module.exports = {
 	id: "Phone-Install-Step-1",
-	aspectRatio: 2.1,
+	aspectRatio: 2.025,
 	caption: "Add bookmark",
 
 	hideCaption: true,
