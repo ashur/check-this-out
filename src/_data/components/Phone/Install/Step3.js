d87b08e00f83a013077c36ab99deb95a86234224
Ashur Cabrera <hello@ashur.cab>
2021-11-07 16:09:45 -0700

    Step 3 (WIP)

--- src/_data/components/Phone/Install/Step3.js
+++ src/_data/components/Phone/Install/Step3.js
@@ -18,21 +18,41 @@ let frameWrangler = new FrameWrangler({
 	},
 });
 
-// Step 3
+// Bookmarklet widget, copied
 frameWrangler.pause( 1000 );
-frameWrangler.movePointer( 0.56, 0.815, 1000 );
+frameWrangler.movePointer( 0.655, 0.268, 1000 );
 frameWrangler.tap();
 frameWrangler.showTopScreen();
 
 // Safari favorites
-frameWrangler.movePointer( 0.75, 0.815, 1000 );
+frameWrangler.movePointer( 0.56, 0.815, 1000 );
 frameWrangler.setBottomScreenBackground( "install/step-03-03" );
 frameWrangler.tap();
 frameWrangler.hideTopScreen();
 
 // Safari favorites, edit mode
-frameWrangler.movePointer( 0.3, 0.155, 1250 );
+frameWrangler.movePointer( 0.745, 0.815, 1000 );
+frameWrangler.setTopScreenBackground( "install/step-03-04" );
 frameWrangler.tap();
+frameWrangler.showTopScreen();
+
+// Edit favorite
+frameWrangler.movePointer( 0.25, 0.155, 1000 );
+frameWrangler.setBottomScreenBackground( "install/step-03-05" );
+frameWrangler.tap();
+frameWrangler.hideTopScreen();
+
+// Select address field
+frameWrangler.movePointer( 0.5, 0.1925, 1000 );
+frameWrangler.setTopScreenBackground( "install/step-03-06" );
+frameWrangler.tap();
+frameWrangler.showTopScreen();
+
+// Clear address field
+frameWrangler.movePointer( 0.7675, null, 1000 );
+frameWrangler.setBottomScreenBackground( "install/step-03-07" );
+frameWrangler.tap();
+frameWrangler.hideTopScreen();
 
 frameWrangler.pause( 1000 );
 
