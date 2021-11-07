032d6f3d47ae14d8aa560ae6f15847c0a8dce624
Ashur Cabrera <hello@ashur.cab>
2021-11-07 16:55:39 -0700

    Additional Step 3 frames

--- src/_data/components/Phone/Install/Step3.js
+++ src/_data/components/Phone/Install/Step3.js
@@ -25,13 +25,13 @@ frameWrangler.tap();
 frameWrangler.showTopScreen();
 
 // Safari favorites
-frameWrangler.movePointer( 0.56, 0.815, 1000 );
+frameWrangler.movePointer( 0.56, 0.8175, 1000 );
 frameWrangler.setBottomScreenBackground( "install/step-03-03" );
 frameWrangler.tap();
 frameWrangler.hideTopScreen();
 
 // Safari favorites, edit mode
-frameWrangler.movePointer( 0.745, 0.815, 1000 );
+frameWrangler.movePointer( 0.745, null, 1000 );
 frameWrangler.setTopScreenBackground( "install/step-03-04" );
 frameWrangler.tap();
 frameWrangler.showTopScreen();
@@ -52,6 +52,31 @@ frameWrangler.showTopScreen();
 frameWrangler.movePointer( 0.7675, null, 1000 );
 frameWrangler.setBottomScreenBackground( "install/step-03-07" );
 frameWrangler.tap();
+frameWrangler.hideTopScreen();
+
+// Show paste menu
+frameWrangler.movePointer( 0.15, null, 1000 );
+frameWrangler.setTopScreenBackground( "install/step-03-08" );
+frameWrangler.tap();
+frameWrangler.showTopScreen();
+
+// Paste
+frameWrangler.movePointer( null, 0.1425, 1000 );
+frameWrangler.setBottomScreenBackground( "install/step-03-09" );
+frameWrangler.tap();
+frameWrangler.hideTopScreen();
+
+// Done
+frameWrangler.movePointer( 0.71, 0.768, 1000 );
+frameWrangler.setTopScreenBackground( "install/step-03-04" );
+frameWrangler.tap();
+frameWrangler.showTopScreen();
+
+// Dismiss sheet
+frameWrangler.movePointer( 0.4, 0.0525, 1000 );
+frameWrangler.setBottomScreenBackground( "install/step-03-01" );
+frameWrangler.tapDown( 250 );
+frameWrangler.movePointer( null, 0.5, 250 );
 frameWrangler.hideTopScreen();
 
 frameWrangler.pause( 1000 );
