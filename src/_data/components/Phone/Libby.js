c58cbc710fd68745ed4c9d2416467a935a792632
Ashur Cabrera <hello@ashur.cab>
2021-11-07 22:04:10 -0700

    Libby demo metrics

--- src/_data/components/Phone/Libby.js
+++ src/_data/components/Phone/Libby.js
@@ -12,41 +12,41 @@ let frameWrangler = new FrameWrangler({
 		},
 
 		pointer: {
-			x: -0.04,
-			y: 0.39
-		}
+			x: 0.37,
+			y: 0.43,
+		},
 	},
 });
 
 frameWrangler.pause( 1000 );
+
+frameWrangler.movePointer( -0.04, 0.39, 1250 );
 frameWrangler.tapDown( 750 );
 frameWrangler.movePointer( 0.4, null, 900 );
 frameWrangler.showTopScreen();
 
+frameWrangler.pause( 250 );
 frameWrangler.setBottomScreenBackground( "screen-gideon-3" );
-frameWrangler.tapUp( 150 );
+frameWrangler.tapUp();
 frameWrangler.hideTopScreen();
 
 frameWrangler.setTopScreenBackground( "screen-gideon-4" );
 frameWrangler.movePointer( null, 0.33, 1000 );
-frameWrangler.tap( 150 );
+frameWrangler.tap();
 frameWrangler.showTopScreen();
 
 frameWrangler.setBottomScreenBackground( "screen-gideon-5" );
 frameWrangler.pause( 250 );
 frameWrangler.movePointer( 0.13, 0.735, 1250 );
-frameWrangler.tap( 150 );
+frameWrangler.tap();
 frameWrangler.hideTopScreen();
 
 frameWrangler.movePointer( 0.425, 0.582, 1500 );
 frameWrangler.setTopScreenBackground( "screen-gideon-6" );
-frameWrangler.tap( 150 );
-frameWrangler.showTopScreen();
-frameWrangler.setBottomScreenBackground( "screen-gideon-1" );
-frameWrangler.pause( 1000 );
-frameWrangler.movePointer( -0.0075, 0.0075, 1500 );
 frameWrangler.tap();
-frameWrangler.hideTopScreen();
+frameWrangler.showTopScreen();
+
+frameWrangler.pause( 2000 );
 
 module.exports = {
 	id: "Phone-Demo",
