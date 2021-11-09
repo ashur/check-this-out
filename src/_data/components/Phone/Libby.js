96c558cc684e30c3b8893e185e5992718fd00a1c
Ashur Cabrera <hello@ashur.cab>
2021-11-08 21:54:55 -0700

    Convert Libby demo to bookmarklet

--- src/_data/components/Phone/Libby.js
+++ src/_data/components/Phone/Libby.js
@@ -3,12 +3,14 @@ const FrameWrangler = require( "../../../utils/frame-wrangler" );
 let frameWrangler = new FrameWrangler({
 	screens: {
 		top: {
-			background: "screen-gideon-2",
+			background: "index/libby-02",
 			opacity: 0,
+			translateX: 0,
 		},
 		bottom: {
-			background: "screen-gideon-1",
+			background: "index/libby-01",
 			opacity: 1,
+			translateX: 0,
 		},
 
 		pointer: {
@@ -18,39 +20,42 @@ let frameWrangler = new FrameWrangler({
 	},
 });
 
-frameWrangler.pause( 1000 );
-
-frameWrangler.movePointer( -0.04, 0.39, 1250 );
+// Select text
+frameWrangler.movePointer( -0.04, 0.38, 1000 );
 frameWrangler.tapDown( 750 );
 frameWrangler.movePointer( 0.4, null, 900 );
-frameWrangler.showTopScreen();
-
-frameWrangler.pause( 250 );
-frameWrangler.setBottomScreenBackground( "screen-gideon-3" );
 frameWrangler.tapUp();
-frameWrangler.hideTopScreen();
-
-frameWrangler.setTopScreenBackground( "screen-gideon-4" );
-frameWrangler.movePointer( null, 0.33, 1000 );
-frameWrangler.tap();
 frameWrangler.showTopScreen();
 
-frameWrangler.setBottomScreenBackground( "screen-gideon-5" );
-frameWrangler.pause( 250 );
-frameWrangler.movePointer( 0.13, 0.735, 1250 );
+// Show bookmarks
+frameWrangler.setBottomScreenBackground( "index/libby-03" );
+frameWrangler.movePointer( 0.5575, 0.8475, 1000 );
 frameWrangler.tap();
 frameWrangler.hideTopScreen();
+frameWrangler.clearTopScreenBackground();
 
-frameWrangler.movePointer( 0.425, 0.582, 1500 );
-frameWrangler.setTopScreenBackground( "screen-gideon-6" );
-frameWrangler.tap();
+// Launch Libby
+frameWrangler.setTopScreenTranslateX( "100%" );
+frameWrangler.setTopScreenBackground( "index/libby-04" );
+frameWrangler.movePointer( 0.135, 0.16, 250 );
 frameWrangler.showTopScreen();
+frameWrangler.tap();
+frameWrangler.setBottomScreenTranslateX( "-100%" );
+frameWrangler.setTopScreenTranslateX( 0 );
+
+// Libby, search results
+frameWrangler.pause( 1000 );
+frameWrangler.setBottomScreenBackground( "index/libby-05" );
+frameWrangler.setBottomScreenTranslateX( 0 );
+frameWrangler.movePointer( 0.415, 0.51, 1000 );
+frameWrangler.tap();
+frameWrangler.hideTopScreen();
 
 frameWrangler.pause( 2000 );
 
 module.exports = {
 	id: "Phone-Demo",
-	aspectRatio: 1.8,
+	aspectRatio: 2.025,
 	caption: "Select the title or author, choose <b>Check This Out</b>, and jump straight to a search in Libby",
 
 	autoplay: false,
